3a2755a6b345d0e8af18c5330945f4c036efa71d
Gonzalo Duran <gon.duran@duocuc.cl>
2024-05-20 13:16:51 -0400

    entrega semana 1

--- www.AlimentoMental.cl/script.js
+++ www.AlimentoMental.cl/script.js
@@ -8,3 +8,30 @@ function mostrarDetalles(titulo, descripcion, imagen) {
 function cerrarDetalles() {
     document.getElementById('detalles').style.display = 'none';
 }
+
+function mostrar(element){
+    // Mostrar la pantalla de carga
+    document.getElementById('loading').style.display = 'flex';
+
+    // Simular un retardo para la carga (por ejemplo, 1 segundo)
+    setTimeout(function() {
+        // Ocultar todos los artículos
+        document.getElementById('home_div').style.display = 'none';
+        document.getElementById('product_div').style.display = 'none';
+        
+        // Mostrar el artículo correspondiente
+        switch (element.id) {
+            case 'home':
+                document.getElementById('home_div').style.display = 'block';
+                break;
+            case 'product':
+                document.getElementById('product_div').style.display = 'block';
+                break;
+        }
+
+        // Ocultar la pantalla de carga
+        document.getElementById('loading').style.display = 'none';
+    }, 1000); // 1000 milisegundos = 1 segundo
+
+
+}
