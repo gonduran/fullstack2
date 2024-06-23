55efa5caedcb5ecaa6a7987af45f8039f0d60fa7
Gonzalo Duran <gon.duran@duocuc.cl>
2024-06-23 15:52:06 -0400

    entrega sumativa 2 semana 6 fullstack 2

---
+++ PuenteMagicoWeb/documentation/js/menu-wc.js
@@ -0,0 +1,228 @@
+'use strict';
+
+customElements.define('compodoc-menu', class extends HTMLElement {
+    constructor() {
+        super();
+        this.isNormalMode = this.getAttribute('mode') === 'normal';
+    }
+
+    connectedCallback() {
+        this.render(this.isNormalMode);
+    }
+
+    render(isNormalMode) {
+        let tp = lithtml.html(`
+        <nav>
+            <ul class="list">
+                <li class="title">
+                    <a href="index.html" data-type="index-link">puente-magico-web documentation</a>
+                </li>
+
+                <li class="divider"></li>
+                ${ isNormalMode ? `<div id="book-search-input" role="search"><input type="text" placeholder="Type to search"></div>` : '' }
+                <li class="chapter">
+                    <a data-type="chapter-link" href="index.html"><span class="icon ion-ios-home"></span>Getting started</a>
+                    <ul class="links">
+                        <li class="link">
+                            <a href="overview.html" data-type="chapter-link">
+                                <span class="icon ion-ios-keypad"></span>Overview
+                            </a>
+                        </li>
+                        <li class="link">
+                            <a href="index.html" data-type="chapter-link">
+                                <span class="icon ion-ios-paper"></span>README
+                            </a>
+                        </li>
+                                <li class="link">
+                                    <a href="dependencies.html" data-type="chapter-link">
+                                        <span class="icon ion-ios-list"></span>Dependencies
+                                    </a>
+                                </li>
+                                <li class="link">
+                                    <a href="properties.html" data-type="chapter-link">
+                                        <span class="icon ion-ios-apps"></span>Properties
+                                    </a>
+                                </li>
+                    </ul>
+                </li>
+                    <li class="chapter">
+                        <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#components-links"' :
+                            'data-bs-target="#xs-components-links"' }>
+                            <span class="icon ion-md-cog"></span>
+                            <span>Components</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse " ${ isNormalMode ? 'id="components-links"' : 'id="xs-components-links"' }>
+                            <li class="link">
+                                <a href="components/AdminClientManagementComponent.html" data-type="entity-link" >AdminClientManagementComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/AdminContactManagementComponent.html" data-type="entity-link" >AdminContactManagementComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/AdminLoginComponent.html" data-type="entity-link" >AdminLoginComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/AdminLogoutComponent.html" data-type="entity-link" >AdminLogoutComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/AdminOrderMonitoringComponent.html" data-type="entity-link" >AdminOrderMonitoringComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/AdminUserManagementComponent.html" data-type="entity-link" >AdminUserManagementComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/AppComponent.html" data-type="entity-link" >AppComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/CartComponent.html" data-type="entity-link" >CartComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/ContactComponent.html" data-type="entity-link" >ContactComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/HomeComponent.html" data-type="entity-link" >HomeComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/LoginComponent.html" data-type="entity-link" >LoginComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/LogoutComponent.html" data-type="entity-link" >LogoutComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/PasswordRecoveryComponent.html" data-type="entity-link" >PasswordRecoveryComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/ProductCatalogComponent.html" data-type="entity-link" >ProductCatalogComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/ProductDetailAluminioComponent.html" data-type="entity-link" >ProductDetailAluminioComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/ProductDetailCroqueraComponent.html" data-type="entity-link" >ProductDetailCroqueraComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/ProductDetailCuadernoComponent.html" data-type="entity-link" >ProductDetailCuadernoComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/ProductDetailLibretaComponent.html" data-type="entity-link" >ProductDetailLibretaComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/ProductDetailPoleraComponent.html" data-type="entity-link" >ProductDetailPoleraComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/ProductDetailTablaComponent.html" data-type="entity-link" >ProductDetailTablaComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/ProfileComponent.html" data-type="entity-link" >ProfileComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/RegisterComponent.html" data-type="entity-link" >RegisterComponent</a>
+                            </li>
+                        </ul>
+                    </li>
+                        <li class="chapter">
+                            <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#injectables-links"' :
+                                'data-bs-target="#xs-injectables-links"' }>
+                                <span class="icon ion-md-arrow-round-down"></span>
+                                <span>Injectables</span>
+                                <span class="icon ion-ios-arrow-down"></span>
+                            </div>
+                            <ul class="links collapse " ${ isNormalMode ? 'id="injectables-links"' : 'id="xs-injectables-links"' }>
+                                <li class="link">
+                                    <a href="injectables/ContactsService.html" data-type="entity-link" >ContactsService</a>
+                                </li>
+                                <li class="link">
+                                    <a href="injectables/CryptoService.html" data-type="entity-link" >CryptoService</a>
+                                </li>
+                                <li class="link">
+                                    <a href="injectables/CustomersService.html" data-type="entity-link" >CustomersService</a>
+                                </li>
+                                <li class="link">
+                                    <a href="injectables/NavigationService.html" data-type="entity-link" >NavigationService</a>
+                                </li>
+                                <li class="link">
+                                    <a href="injectables/OrdersService.html" data-type="entity-link" >OrdersService</a>
+                                </li>
+                                <li class="link">
+                                    <a href="injectables/UsersService.html" data-type="entity-link" >UsersService</a>
+                                </li>
+                            </ul>
+                        </li>
+                    <li class="chapter">
+                        <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#interfaces-links"' :
+                            'data-bs-target="#xs-interfaces-links"' }>
+                            <span class="icon ion-md-information-circle-outline"></span>
+                            <span>Interfaces</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse " ${ isNormalMode ? ' id="interfaces-links"' : 'id="xs-interfaces-links"' }>
+                            <li class="link">
+                                <a href="interfaces/Cart.html" data-type="entity-link" >Cart</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/CartItem.html" data-type="entity-link" >CartItem</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/Contact.html" data-type="entity-link" >Contact</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/Contact-1.html" data-type="entity-link" >Contact</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/Customer.html" data-type="entity-link" >Customer</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/Customer-1.html" data-type="entity-link" >Customer</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/Order.html" data-type="entity-link" >Order</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/OrderDetail.html" data-type="entity-link" >OrderDetail</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/Profile.html" data-type="entity-link" >Profile</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/User.html" data-type="entity-link" >User</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/User-1.html" data-type="entity-link" >User</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/Users.html" data-type="entity-link" >Users</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="chapter">
+                        <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#miscellaneous-links"'
+                            : 'data-bs-target="#xs-miscellaneous-links"' }>
+                            <span class="icon ion-ios-cube"></span>
+                            <span>Miscellaneous</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse " ${ isNormalMode ? 'id="miscellaneous-links"' : 'id="xs-miscellaneous-links"' }>
+                            <li class="link">
+                                <a href="miscellaneous/functions.html" data-type="entity-link">Functions</a>
+                            </li>
+                            <li class="link">
+                                <a href="miscellaneous/variables.html" data-type="entity-link">Variables</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="chapter">
+                        <a data-type="chapter-link" href="coverage.html"><span class="icon ion-ios-stats"></span>Documentation coverage</a>
+                    </li>
+                    <li class="divider"></li>
+                    <li class="copyright">
+                        Documentation generated using <a href="https://compodoc.app/" target="_blank" rel="noopener noreferrer">
+                            <img data-src="images/compodoc-vectorise.png" class="img-responsive" data-type="compodoc-logo">
+                        </a>
+                    </li>
+            </ul>
+        </nav>
+        `);
+        this.innerHTML = tp.strings;
+    }
+});
--- PuenteMagicoWeb/src/app/services/contacts.service.ts
+++ PuenteMagicoWeb/src/app/services/contacts.service.ts
@@ -27,6 +27,17 @@ export class ContactsService {
     }
   }
 
+  /**
+   * @description
+   * Registra un nuevo contacto en la lista y en localStorage.
+   * 
+   * @param {string} name - El nombre del contacto.
+   * @param {string} email - El correo electrónico del contacto.
+   * @param {string} phone - El número de teléfono del contacto.
+   * @param {string} subject - El asunto del contacto.
+   * @param {string} message - El mensaje del contacto.
+   * @return {boolean} - Retorna true si el contacto fue registrado exitosamente, de lo contrario false.
+   */
   registerContacts(name: string, email: string, phone: string, subject: string, message: string): boolean {
     console.log('Intentando registrar contacto cliente:', { name, email, phone, subject, message });
     const fecha = new Date();
@@ -43,6 +54,18 @@ export class ContactsService {
     return true;
   }
 
+  /**
+   * @description
+   * Actualiza un contacto existente en la lista y en localStorage.
+   * 
+   * @param {string} name - El nombre del contacto.
+   * @param {string} email - El correo electrónico del contacto.
+   * @param {string} phone - El número de teléfono del contacto.
+   * @param {string} subject - El asunto del contacto.
+   * @param {string} message - El mensaje del contacto.
+   * @param {number} id - El ID del contacto a actualizar.
+   * @return {boolean} - Retorna true si el contacto fue actualizado exitosamente, de lo contrario false.
+   */
   updateContacts(name: string, email: string, phone: string, subject: string, message: string, id: number): boolean {
     console.log('Intentando actualizar contacto cliente:', { name, email, phone, subject, message, id });
     const contactExisting = this.contacts.find(contact => contact.id === id);
@@ -70,6 +93,13 @@ export class ContactsService {
     return false;
   }
 
+  /**
+   * @description
+   * Muestra una alerta en la interfaz de usuario.
+   * 
+   * @param {string} mensaje - El mensaje de la alerta.
+   * @param {string} tipo - El tipo de alerta (e.g., 'success', 'danger').
+   */
   private mostrarAlerta(mensaje: string, tipo: string): void {
     const alertaDiv = document.createElement('div');
     alertaDiv.className = `alert alert-${tipo}`;
@@ -92,6 +122,12 @@ export class ContactsService {
     }
   }
 
+  /**
+   * @description
+   * Verifica si localStorage está disponible.
+   * 
+   * @return {boolean} - Retorna true si localStorage está disponible, de lo contrario false.
+   */
   isLocalStorageAvailable(): boolean {
     try {
       const test = '__test__';
@@ -103,6 +139,13 @@ export class ContactsService {
     }
   }
 
+  /**
+   * @description
+   * Busca un contacto por ID.
+   * 
+   * @param {number} id - El ID del contacto a buscar.
+   * @return {boolean} - Retorna true si el contacto fue encontrado, de lo contrario false.
+   */
   findContact(id: number): boolean {
     console.log('Buscando contacto cliente:', { id });
     const contact = this.contacts.find(contact => contact.id === id);
@@ -117,32 +160,71 @@ export class ContactsService {
     }
   }
 
+  /**
+   * @description
+   * Formatea una fecha en formato YYYY-MM-DD a DD-MM-YYYY.
+   * 
+   * @param {string} date - La fecha en formato YYYY-MM-DD.
+   * @return {string} - La fecha formateada en formato DD-MM-YYYY.
+   */
   private formatToFormDate(date: string): string {
     const [year, month, day] = date.split('-');
     return `${day}-${month}-${year}`;
   }
 
+  /**
+   * @description
+   * Formatea una fecha en formato DD-MM-YYYY a YYYY-MM-DD.
+   * 
+   * @param {string} date - La fecha en formato DD-MM-YYYY.
+   * @return {string} - La fecha formateada en formato YYYY-MM-DD.
+   */
   private formatToStorageDate(date: string): string {
     const [day, month, year] = date.split('-');
     return `${year}-${month}-${day}`;
   }
 
+  /**
+   * @description
+   * Obtiene todos los contactos almacenados en localStorage.
+   * 
+   * @return {Contact[]} - Retorna un arreglo de contactos.
+   */
   getContacts(): Contact[] {
     return JSON.parse(localStorage.getItem(this.storageKey) || '[]');
   }
 
+  /**
+   * @description
+   * Agrega un nuevo contacto a la lista y a localStorage.
+   * 
+   * @param {Contact} contact - El contacto a agregar.
+   */
   addContact(contact: Contact): void {
     const contacts = this.getContacts();
     contacts.push(contact);
     localStorage.setItem(this.storageKey, JSON.stringify(contacts));
   }
 
+  /**
+   * @description
+   * Actualiza un contacto existente en la lista y en localStorage.
+   * 
+   * @param {number} index - El índice del contacto a actualizar.
+   * @param {Contact} updatedContact - El contacto actualizado.
+   */
   updateContact(index: number, updatedContact: Contact): void {
     const contacts = this.getContacts();
     contacts[index] = updatedContact;
     localStorage.setItem(this.storageKey, JSON.stringify(contacts));
   }
 
+  /**
+   * @description
+   * Elimina un contacto de la lista y de localStorage.
+   * 
+   * @param {number} index - El índice del contacto a eliminar.
+   */
   deleteContact(index: number): void {
     const contacts = this.getContacts();
     contacts.splice(index, 1);
