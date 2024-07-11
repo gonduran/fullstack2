11dd3ce73596ae21743981b4b3a64e47a0844194
Gonzalo Duran <gon.duran@duocuc.cl>
2024-07-11 19:51:14 -0400

    se mueven a respaldo

--- www.Puente-Magico.cl/admin/js/index.js
+++
@@ -1,6 +0,0 @@
-var menuToggle = document.getElementById("menu-toggle");
-var wrapper = document.getElementById("wrapper");
-
-menuToggle.addEventListener("click", function() {
-    wrapper.classList.toggle("toggled");
-});
